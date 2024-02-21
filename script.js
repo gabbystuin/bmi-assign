62f74c987aedc1478cad96c7952493e00f6f2e6c
Your Name <you@example.com>
2024-02-21 13:20:12 +0100

    my first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+const ul =document.querySelector("ul")
+
+const myArray= [
+    "Enter the dragon (2024)",
+    "Mashle(2024)",
+    "Legend of kora(2004)",
+    " the hulk(2019)",
+    "tornado(2021)",
+    "Avengers end game(2024)",
+    "The brotherhod(2024)",
+]
+
+    
